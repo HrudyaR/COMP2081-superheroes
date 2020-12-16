ab90409888a0d7fb7c88fbace9280163eb5b2ab8
HrudyaR <hrudya.jayaprakash@gmail.com>
2020-12-16 14:38:41 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="mw6 center">
+      <!-- <a
+        class="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib dark-blue"
+        href="#0"
+        @click="addHero"
+        >Add Hero</a
+      > -->
+      <add-hero-button text="Add Hero" @add-hero="addHero"></add-hero-button>
+      <twitter-short-profile
+        v-for="hero in heroes"
+        :key="hero.name"
+        :superhero="hero"
+      ></twitter-short-profile>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TwitterShortProfile from "./components/TwitterShortProfile";
+import AddHeroButton from "./components/AddHeroButton";
+const heroes = [
+  {
+    name: "Prof. Xavier",
+    twitter: "@profx",
+    pic:
+      "http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0004.gif",
+  },
+  {
+    name: "Spiderman",
+    twitter: "@spidey",
+    pic:
+      "http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0008.gif",
+  },
+  {
+    name: "Wolverine",
+    pic:
+      "http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0011.gif",
+    twitter: "@logan",
+  },
+  {
+    name: "Cyclops",
+    twitter: "@oneye",
+    pic:
+      "http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0005.gif",
+  },
+  {
+    name: "Storm",
+    twitter: "@rainsitpours",
+    pic:
+      "http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0007.gif",
+  },
+  {
+    name: "Phoenix",
+    twitter: "@jeangrey",
+    pic:
+      "http://www.animatedimages.org/data/media/450/animated-marvel-avatar-image-0016.gif",
+  },
+];
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TwitterShortProfile,
+    AddHeroButton,
+  },
+  data: function() {
+    return {
+      storage: heroes,
+      heroes: [],
+    };
+  },
+  methods: {
+    addHero() {
+      // if (this.storage.length) {
+      //   this.heroes.push(this.storage.pop());
+      // }
+      this.storage.length > 0 && this.heroes.push(this.storage.pop());
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Tachyons from "tachyons";
 
-Vue.config.productionTip = false
+Vue.use(Tachyons);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
